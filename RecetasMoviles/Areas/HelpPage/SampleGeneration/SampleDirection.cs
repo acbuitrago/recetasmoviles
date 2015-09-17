f05f7124365423e43a5b59ff7e77e27895f48745
Lina <lina.m8a@gmail.com>
2015-09-16 22:17:16 -0500

    Primer commit

---
+++ RecetasMoviles/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace RecetasMoviles.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
---
+++ RecetasMoviles/Controllers/RecetasController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RecetasMoviles.Controllers
+{
+    public class RecetasController : ApiController
+    {
+        public string[] Get()
+        {
+            Random random = new Random();
+            int value = random.Next(0, 100);
+
+            return new string[]
+                {
+                     "value",
+                     value.ToString()
+                };
+        }
+    }
+}
